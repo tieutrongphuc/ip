8c186332ff9d9ae7b10ffc055f7eb0b5533c102f
phuxtrohhg <phuctrong572006@gmail.com>
2025-08-21 21:24:10 +0800

    Add ability to store text enter by user
    Using nested if else for checking list and bye case

--- src/main/java/July.java
+++ src/main/java/July.java
@@ -3,15 +3,26 @@ import java.util.Scanner;
 public class July {
     public static void main(String[] args) {
         System.out.println("Hello I'm July, your helpful assistant");
+
         Scanner scanner = new Scanner(System.in);
         String input;
+        int listLength = 0;
+        String[] list = new String[105];
 
         while (true) {
             input = scanner.nextLine();
             if (input.equalsIgnoreCase("bye")) {
                 break;
             } else {
-                System.out.println(input);
+                if (input.equalsIgnoreCase("list")) {
+                    for (int i = 0; i < listLength; i++) {
+                        System.out.printf("%d.%s%n", i + 1, list[i]);
+                    }
+                } else {
+                    System.out.println("added: " + input);
+                    list[listLength] = input;
+                    listLength++;
+                }
             }
         }
 
