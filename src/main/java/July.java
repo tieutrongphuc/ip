3adb912628b41d79fe74c3f7b528958523562875
phuxtrohhg <phuctrong572006@gmail.com>
2025-08-21 19:33:14 +0800

    Rename chatbot to July.
    Add greets and exit sentences.

---
+++ src/main/java/July.java
@@ -0,0 +1,6 @@
+public class July {
+    public static void main(String[] args) {
+        System.out.println("Hello I'm July, your helpful assistant");
+        System.out.println("Good bye, I'm always here for you.");
+    }
+}
