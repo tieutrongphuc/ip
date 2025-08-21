b322363813b43a4ee6030c037251af12745d75cd
phuxtrohhg <phuctrong572006@gmail.com>
2025-08-21 19:43:20 +0800

    Add exit when say 'bye'
    Echo anything say by user

--- src/main/java/July.java
+++ src/main/java/July.java
@@ -1,6 +1,21 @@
+import java.util.Scanner;
+
 public class July {
     public static void main(String[] args) {
         System.out.println("Hello I'm July, your helpful assistant");
+        Scanner scanner = new Scanner(System.in);
+        String input;
+
+        while (true) {
+            input = scanner.nextLine();
+            if (input.equalsIgnoreCase("bye")) {
+                break;
+            } else {
+                System.out.println(input);
+            }
+        }
+
+        scanner.close();
         System.out.println("Good bye, I'm always here for you.");
     }
 }
